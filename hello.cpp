3e888b793d8a9e5c090d0423170bf74dca8ea717
1ppo <ippokratiskedioglou@me.com>
2015-09-09 14:49:23 -0400

    i changed some stuff
    yeah!

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 
+string name
+
 int main () {
   
-  cout << "Hello Class!\n\n";
-	cout << "poop"
+  cout << "Your name is bob. Enter bob.\n\n";
+	cin >> name
+	cout << "Hello bob my name is alice. \n\n"
+	
 
   return 0;
 }
